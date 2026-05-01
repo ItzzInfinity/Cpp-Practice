382a51ae3d0eb567711b606f42334c89af1128e6
Anjan Prasad <prasadanjan25@gmail.com>
2026-05-01 11:25:49 +0530

    section 1 correted

--- section_01.cpp
+++ section_01.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "hello World!"<<endl; // Write a program that prints Hello, World!
-    cout << "Anjan Prasad"<<endl; // Print your name using cout
+    cout << "hello World!" << endl; // Write a program that prints Hello, World!
+    cout << "Anjan Prasad" << endl; // Print your name using cout
     // ^^^ Print two lines of text using two cout statements
-    cout << 42<<endl; // Print a number (e.g. `42`) using `cout`
-    cout << 3.14<<endl; // Print a decimal number (e.g. `3.14`) using `cout`
-    cout <<'A'<<endl; // Print a single character (e.g. `'A'`) using `cout`
-    cout << "Itzz\tInfinity"<< endl; // Print text with a tab space between two words using `\t`
-    cout << "this should be in \nNew line"<< endl; // Print text on a new line using `\n`
+    cout << 42 << endl; // Print a number (e.g. `42`) using `cout`
+    cout << 3.14 << endl; // Print a decimal number (e.g. `3.14`) using `cout`
+    cout << 'A' << endl; // Print a single character (e.g. `'A'`) using `cout`
+    cout << "Itzz\tInfinity" << endl; // Print text with a tab space between two words using `\t`
+    cout << "this should be in " << endl << "New line" << endl; // Print text on a new line using `\n`
     // ^^^ Print text on a new line using `endl`
-    cout << "\n\nThis should be a blank line" << endl;// Print a blank line between two sentences
-    cout << 5+3 <<endl; // Print the result of `5 + 3` directly inside `cout`
+    cout << "This should create a " << endl;// Print a blank line between two sentences
+    cout << endl;// Print a blank line between two sentences
+    cout << "blank line between these sentences" << endl;// Print a blank line between two sentences
+    cout << 5+3 << endl; // Print the result of `5 + 3` directly inside `cout`
     cout << "this" << " is a " << "text " << "with '<<' chaining" << endl; // Print multiple things in one `cout` using `<<` chaining
     cout << "My name is Anjan Prasad" << endl << "And I am 26 years old"<< endl; // Print your full name and age on separate lines
     cout << "This is written in \"C++\" Language" << endl; // Print a sentence that includes a double quote character (use `\"`)
