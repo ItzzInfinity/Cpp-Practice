2437865fc9d92a7debb2c2761e381e41d125580d
Anjan Prasad <prasadanjan25@gmail.com>
2026-05-03 17:12:40 +0530

    section 3 update

--- section_03.cpp
+++ section_03.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-
+using namespace std;
 int main(){
 
 // Task 01 Read an integer from the user and print it back
-
-
-
+    int num1;
+    cout << "Enter the number you want : ";
+    cin >> num1;
+    cout << "you entered : " << num1 << endl;
 // Task 02 Read a float from the user and print it back
-
-
-
+    float num2;
+    cout << "Enter the float you want : ";
+    cin >> num2;
+    cout << "you entered : " << num2 << endl;
 // Task 03 Read a character from the user and print it back
-
-
-
+    char char1;
+    cout << "Enter the Character you want : ";
+    cin >> char1;
+    cout << "you entered : " << char1 << endl;
 // Task 04 Read a string (single word) using `cin` and print it
-
-
-
+    char char1;
+    cout << "Enter the Character you want : ";
+    cin >> char1;
+    cout << "you entered : " << char1 << endl;
 // Task 05 Read a full line of text using `getline()` and print it
+
 
 
 
@@ -26,7 +31,9 @@ int main(){
 
 
 
+
 // Task 07 Read the user's name and greet them: `Hello, [name]!`
+
 
 
 
@@ -34,10 +41,16 @@ int main(){
 
 
 
+
 // Task 09 Read a number and print it doubled
 
 
 
+
 // Task 10 Read age from user and print: `You are [age] years old.`
-    return 0;
+
+
+
+
+return 0;
 }
