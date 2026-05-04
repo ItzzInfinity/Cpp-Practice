144de7243930ac71bf9e65e467af05145d59967d
Anjan Prasad <prasadanjan25@gmail.com>
2026-05-04 12:29:31 +0530

    section 3 complete

--- section_03.cpp
+++ section_03.cpp
@@ -3,53 +3,62 @@ using namespace std;
 int main(){
 
 // Task 01 Read an integer from the user and print it back
-    int num1;
-    cout << "Enter the number you want : ";
-    cin >> num1;
-    cout << "you entered : " << num1 << endl;
+    // int num1;
+    // cout << "Enter the number you want : ";
+    // cin >> num1;
+    // cout << "you entered : " << num1 << endl;
 // Task 02 Read a float from the user and print it back
-    float num2;
-    cout << "Enter the float you want : ";
-    cin >> num2;
-    cout << "you entered : " << num2 << endl;
-// Task 03 Read a character from the user and print it back
-    char char1;
-    cout << "Enter the Character you want : ";
-    cin >> char1;
-    cout << "you entered : " << char1 << endl;
-// Task 04 Read a string (single word) using `cin` and print it
-    char char1;
-    cout << "Enter the Character you want : ";
-    cin >> char1;
-    cout << "you entered : " << char1 << endl;
+    // float num2;
+    // cout << "Enter the float you want : ";
+    // cin >> num2;
+    // cout << "you entered : " << num2 << endl;
+    // Task 03 Read a character from the user and print it back
+    // char char1;
+    // cout << "Enter the Character you want : ";
+    // cin >> char1;
+    // cout << "you entered : " << char1 << endl;
+    // Task 04 Read a string (single word) using `cin` and print it
+    // string str1;
+    // cout << "Enter the string you want : ";
+    // cin >> str1;
+    // cout << "you entered : " << str1 << endl;
 // Task 05 Read a full line of text using `getline()` and print it
-
-
-
-
+    // string str2;
+    // cout << "Enter the string you want : ";
+    // getline(cin, str2);
+    // cout << "you entered using getline() : " << str2 << endl;
 // Task 06 Read two integers from the user and print both
-
-
-
-
+    // int int1, int2;
+    // cout << "Enter two integers you want : ";
+    // cin >> int1 >> int2;
+    // cout << "you entered : " << int1 << " & " << int2 << endl;
 // Task 07 Read the user's name and greet them: `Hello, [name]!`
-
-
-
-
+    // string str3;
+    // cout << "Enter your Name : ";
+    // getline(cin, str3);
+    // cout << "Hello " << str3 << "! How are you?" << endl;
 // Task 08 Read two numbers and print their sum
-
-
-
-
+    // double num3, num4;
+    // cout << "Enter two integers you want : " << endl ;
+    // cout << "Enter the first one : ";
+    // cin >> num3;
+    // cout << "Enter the second one : ";
+    // cin >> num4;
+    // cout << "The sum of your numbers are : " << num3 + num4 << endl;
 // Task 09 Read a number and print it doubled
-
-
-
-
-// Task 10 Read age from user and print: `You are [age] years old.`
-
-
+    // double num5;
+    // unsigned char uChar;
+    // cout << "Enter a number you want : " << endl ;
+    // cin >> num5;
+    // cout << "The twice of your number is : " << num5 * 2 << endl;
+    // uChar = num5;
+    // cout << "The twice of your number is : " << (uChar << 1) << endl;
+    // Task 10 Read age from user and print: `You are [age] years old.`
+    int int3;
+    cout << "Enter your age : " << endl ;
+    cin >> int3;
+    cout << "You are " << int3 << " years old" << endl ;
+    
 
 
 return 0;
